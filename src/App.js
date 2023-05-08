632a6cde08a41b2852c6cca861050ba0b73293ce
unknown <mclaborn2010@gmail.com>
2023-05-08 14:02:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import ProfileCard from './ProfileCard';
-import AlexaImage from './Project Resources/alexa.png'
-import SiriImage from './Project Resources/siri.png'
-import CortanaImage from './Project Resources/cortana.png'
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import LeelaImage from "./Project Resources/leela.png";
+import FryImage from "./Project Resources/fry1.png";
+import BenderImage from "./Project Resources/bender.png";
 
-function App(){
-    return (
-        <div>
-            <div>Personal Digital Assistants</div>
-
-
-            <ProfileCard title='Alexa' handle="@alexa99" image = { AlexaImage }/>
-            <ProfileCard title='Cortana' handle="@cortana32" image = { SiriImage }/>
-            <ProfileCard title='Siri' handle="@Siri75" image = { CortanaImage } />
+function App() {
+  return (
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">The Best of Futurama</p>
         </div>
-    );
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Leela"
+                handle="@leelalovesyou"
+                image={LeelaImage}
+                description="Leela is a badass warrior with one eye!"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Fry"
+                handle="@frymaster3000"
+                image={FryImage}
+                description="Fry is a pizza delivery man turned delivery man after accidentally locking himself in a cryo-chamber for a thousand years"
+
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Bender"
+                handle="@blackjackislife"
+                image={BenderImage}
+                description="Bender is a complete degenerate that somehow stays on the team due to his charm, humor, and occasional usefulness. "
+
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
